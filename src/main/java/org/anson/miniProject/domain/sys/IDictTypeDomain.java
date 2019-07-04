cee39bbc40a3bdf9925be53e3fd922c9018c9fcf
wanganxiong <wanganxiong@uni-top.com>
2019-07-04 13:35:50 +0800

    for save

--- src/main/java/org/anson/miniProject/domain/sys/IDictTypeDomain.java
+++ src/main/java/org/anson/miniProject/domain/sys/IDictTypeDomain.java
@@ -2,16 +2,11 @@ package org.anson.miniProject.domain.sys;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.anson.miniProject.model.bo.sys.DictTypeBo;
-import org.springframework.validation.annotation.Validated;
 
-import javax.validation.Valid;
 import java.util.Date;
 
-@Validated
 public interface IDictTypeDomain {
-    @Validated(DictTypeBo.add.class)
-    String add(@Valid DictTypeBo bo, String operUserId, Date operTime);
-    @Validated(DictTypeBo.mdf.class)
-    void mdf(@Valid DictTypeBo bo, String operUserId, Date operTime);
+    String add(DictTypeBo bo, String operUserId, Date operTime);
+    void mdf(DictTypeBo bo, String operUserId, Date operTime);
     void del(String dictTypeId, String operUserId, Date operTime) throws JsonProcessingException;
 }
